23079942f4d7db8a79d9563f99373cefa5f29bbb
jordifermo-dev <jordifermo@gmail.com>
2026-08-03 00:27:14 +0200

    Añadir plataforma Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'dancemeet-front',
+  appId: 'com.dancemeet.app',
+  appName: 'DanceMeet',
   webDir: 'www'
 };
 
